d2899eda5f40198d960b45259ca07fb4cff37608
Anne Norling <anne.norling@lnu.se>
2016-03-08 15:38:55 +0100

    Initial commit - solution level B

---
+++ b-niva/losning/DigitalAlarmClock_B/AlarmClock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace DigitalAlarmClock_B
+{
+    class AlarmClock
+    {
+        #region Fält
+
+        private ClockDisplay _alarmTime;
+        private ClockDisplay _time;
+
+        #endregion
+
+        #region Konstruktorer
+
+        public AlarmClock()
+            : this(0, 0)
+        {
+            // Tom!
+        }
+
+        public AlarmClock(int hour, int minute)
+            : this(hour, minute, 0, 0)
+        {
+            // Tom!
+        }
+
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            this._time = new ClockDisplay(hour, minute);
+            this._alarmTime = new ClockDisplay(alarmHour, alarmMinute);
+        }
+
+        #endregion
+
+        #region Egenskaper
+
+        public int AlarmHour
+        {
+            get { return this._alarmTime.Hour; }
+            set { this._alarmTime.Hour = value; }
+        }
+
+        public int AlarmMinute
+        {
+            get { return this._alarmTime.Minute; }
+            set { this._alarmTime.Minute = value; }
+        }
+
+        public int Hour
+        {
+            get { return this._time.Hour; }
+            set { this._time.Hour = value; }
+        }
+
+        public int Minute
+        {
+            get { return this._time.Minute; }
+            set { this._time.Minute = value; }
+        }
+
+        #endregion
+
+        #region Instansmetoder
+
+        public bool TickTock()
+        {
+            this._time.Increment();
+
+            return this._time.ToString() == this._alarmTime.ToString();
+        }
+
+        #endregion
+
+        #region Överskuggade instansmetoder
+
+        public override string ToString()
+        {
+            return String.Format("{0,5} ({1})", this._time, this._alarmTime);
+        }
+
+        #endregion
+    }
+}
+
---
+++ b-niva/losning/DigitalAlarmClock_B/ClockDisplay.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DigitalAlarmClock_B
+{
+    class ClockDisplay
+    {
+        #region Fält
+
+        private NumberDisplay _hourDisplay;
+        private NumberDisplay _minuteDisplay;
+
+        #endregion
+
+        #region Konstruktorer
+
+        public ClockDisplay()
+            : this(0, 0)
+        {
+        }
+
+        public ClockDisplay(int hour, int minute)
+        {
+            this._hourDisplay = new NumberDisplay(23, hour);
+            this._minuteDisplay = new NumberDisplay(59, minute);
+        }
+
+        #endregion
+
+        #region Egenskaper
+
+        public int Hour
+        {
+            get { return this._hourDisplay.Number; }
+            set { this._hourDisplay.Number = value; }
+        }
+
+        public int Minute
+        {
+            get { return this._minuteDisplay.Number; }
+            set { this._minuteDisplay.Number = value; }
+        }
+
+        #endregion
+
+        #region Instansmetoder
+
+        public void Increment()
+        {
+            this._minuteDisplay.Increment();
+            if (this._minuteDisplay.Number == 0)
+            {
+                this._hourDisplay.Increment();
+            }
+        }
+
+        #endregion
+
+        #region Överskuggade metoder
+
+        public override string ToString()
+        {
+            return String.Format("{0}:{1}", this._hourDisplay, this._minuteDisplay.ToString("00"));
+        }
+
+        #endregion
+    }
+}
